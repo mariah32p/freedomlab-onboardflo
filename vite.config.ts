2b5b4d2385cd97670e198715b1c03fe084ac5c33
mariah32p <mariah@32parallels.com>
2025-08-28 23:13:57 -0500

    Fix MIME type issues for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       },
     },
   },
+  server: {
+    fs: {
+      strict: false
+    }
+  }
 })
