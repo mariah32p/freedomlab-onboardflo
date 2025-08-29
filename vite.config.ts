5f74465bac7d3c955cec8a88251103b137208a73
mariah32p <mariah@32parallels.com>
2025-08-28 22:55:35 -0500

    Add TypeScript config and fix asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
-    },
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript',
     },
   },
 })
