03e64235e2d65a11a84c636e74857dcc15f5d90a
mariah32p <mariah@32parallels.com>
2025-08-29 22:56:59 -0500

    Fix useEffect import error

--- src/pages/SubmissionsPage.tsx
+++ src/pages/SubmissionsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { useSubscription } from '../hooks/useSubscription';
 import { useCustomerSessions } from '../hooks/useCustomerSessions';
